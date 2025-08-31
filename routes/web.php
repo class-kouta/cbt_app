29e8f820862204ae1debe1bcd340ee1cc6093db3
class-kouta <ff03csm26.en@gmial.com>
2025-08-31 13:20:23 +0900

    api.phpにすでに記載されているため削除

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::post('/todos', [TodoController::class, 'store']);
